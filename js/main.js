58d8e3e2212f2cd92841656a8d30a1aebd61f806
feffyx <ziacofederica@gmail.com>
2023-11-14 17:00:57 +0100

    completed

--- js/main.js
+++ js/main.js
@@ -1,17 +1,46 @@
-// Chiedi informazioni
-let distance = parseFloat(prompt('Type distance in km'));
-let age = parseInt(prompt('Enter your age'));
+// Variabili
+let distance, age;
 
 // Calcola il prezzo del biglietto
-let price = distance * 0.21;
+function calculateTicketPrice(distance, age) {
+    let price = distance * 0.21;
 
-// Sconti
-if (age < 18) {
-    price -= price * 0.2;
-} else if (age >= 65) {
-    price -= price * 0.4;
+    // Sconti
+    if (age < 18) {
+        price -= price * 0.2;
+    } else if (age >= 65) {
+        price -= price * 0.4;
+    }
+
+    // Restituisce il prezzo
+    return price.toFixed(2);
 }
 
-// Prezzo
-let finalPrice = price.toFixed(2);
+// Costruzione Form
+function formHasBeenCompleted(event) {
+    event.preventDefault();
 
+    // Informazioni
+    let name = document.getElementById('name').value;
+    distance = parseFloat(document.getElementById('distance').value);
+    age = parseInt(document.getElementById('age').value);
+
+    // Calcola il prezzo
+    let price = calculateTicketPrice(distance, age);
+
+    // HTML
+    document.getElementById("nameDisplay").innerText = `Name: ${name}`;
+    document.getElementById("distanceDisplay").innerText = `Distance: ${distance}`;
+    document.getElementById("ageDisplay").innerText = `Age: ${age}`;
+    document.getElementById("priceDisplay").innerText = `Price: ${price}€`;
+
+    // Print on Console
+    let informations = `
+    --- Informazioni Utente ---
+    Distance: ${distance}
+    Age: ${age}
+    Name: ${name}
+    Price : ${price}
+    `;
+    console.log(informations);
+}
